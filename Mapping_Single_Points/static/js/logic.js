2b9467ae7e90576eda6fdeabd27e097f12c4c6ac
Saad Khan <skhan315@gatech.edu>
2022-02-23 17:06:21 -0600

    Adding files for mapping single points

---
+++ Mapping_Single_Points/static/js/logic.js
@@ -0,0 +1,24 @@
+// Create our initial map object
+// Set the longitude, latitude, and the starting zoom level
+
+
+// Add a tile layer (the background map image) to our map
+// We use the addTo method to add objects to our map
+
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with a center and zoom level.
+let map = L.map('mapid').setView([40.7, -94.5], 4);
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
