f3a95c8e7d2ec75add75c33a83093b4391f78160
Saad Khan <skhan315@gatech.edu>
2022-02-25 08:40:42 -0600

    Adding files to the Mapping_Lines folder

---
+++ Mapping_Lines/static/js/logic.js
@@ -0,0 +1,77 @@
+// Create our initial map object
+// Set the longitude, latitude, and the starting zoom level
+
+
+// Add a tile layer (the background map image) to our map
+// We use the addTo method to add objects to our map
+
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+
+// Create the map object with a center and zoom level.
+//let map = L.map('mapid').setView([36.1733, -120.1794], 7);
+//let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+
+// Coordinates for each point to be used in the line.
+let line = [
+    [33.9416, -118.4085],
+    [37.6213, -122.3790]
+  ];
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+    color: "red"
+  }).addTo(map);
+
+
+// Coordinates for each point to be used in the polyline.
+let line2 = [
+    [33.9416, -118.4085],
+    [37.6213, -122.3790],
+    [40.7899, -111.9791],
+    [47.4502, -122.3088]
+  ];
+
+// Create a polyline using the line coordinates and make the line yellow.
+L.polyline(line2, {
+    color: "yellow"
+ }).addTo(map);
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/satellite-streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    //Grayscale: grayscale,
+    accessToken: API_KEY
+});
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
+
+//  Add a marker to the map for Los Angeles, California.
+//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+//let circle = L.circle([34.0522, -118.2437], {
+//    radius: 300
+// }).addTo(map);
+
+
+// Get data from cities.js
+let cityData = cities;
+
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population/100000
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+  .addTo(map);
+});
